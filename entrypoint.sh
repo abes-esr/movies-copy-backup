ce14c017c5174418b5a789fee72c5357f4057b77
Nathanaël Blanchet <blanchet@abes.fr>
2024-01-16 23:55:10 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export KUBECONFIG=/run/secrets/kubeconfig/kubeconfig
+oc project $PROJECT
 export DUMPER=$(oc get pods -o json | jq -r '.items[].metadata|select(.name|test("dumper")).name')
 export WDQS=$(oc get pods -o json | jq -r '.items[].metadata|select((.name|test("wdqs")) and (.name|test("frontend|proxy|updater")|not)).name')
 echo "3    0       *       *       *       oc exec $DUMPER -- restore "'$(ls -t /backup_racine/prod/*.gz | head -n1) $DB_TYPE $DB_HOST $DB_NAME $DB_USER $DB_PASS 3306'" >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
