beb5bfee933e5bd3d0730f5e9c8868256fb0e242
Nathanaël Blanchet <blanchet@abes.fr>
2024-01-17 18:23:26 +0100

    Update oc.sh

--- oc.sh
+++ oc.sh
@@ -2,24 +2,33 @@
 
 oc project $1
 
-case $2 in
-    dumper)
-        export DUMPER=$(oc get pods -o json | jq -r '.items[].metadata|select(.name|test("dumper")).name')
-        echo "3    0       *       *       *       oc exec $DUMPER -- restore "'$(ls -t /backup_racine/prod/*.gz | head -n1) $DB_TYPE $DB_HOST $DB_NAME $DB_USER $DB_PASS 3306'" >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
-        ;;
-    wdqs-jnl)
-        export WDQS=$(oc get pods -o json | jq -r '.items[].metadata|select((.name|test("wdqs")) and (.name|test("frontend|proxy|updater")|not)).name')
-        echo "4    0       *       *       *       oc exec $WDQS -- sh -c 'rm -f data/data.jnl' >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
-        ;;
-    wdqs-run)
-        export WDQS=$(oc get pods -o json | jq -r '.items[].metadata|select((.name|test("wdqs")) and (.name|test("frontend|proxy|updater")|not)).name')
-        echo "5    0       *       *       *       oc exec $WDQS -- bash -c '/wdqs/runUpdate.sh "'-h http://$WDQS_HOST:$WDQS_PORT -- --wikibaseUrl $WIKIBASE_SCHEME://$WIKIBASE_HOST --conceptUri $WIKIBASE_SCHEME://$WIKIBASE_HOST --entityNamespaces ${WDQS_ENTITY_NAMESPACES} -s 20200501000000'"'" "| tee logfile | awk '1;/Got no real changes/{exit}'"" " >> /var/spool/cron/crontabs/root
-        ;;
-    all)
-        export DUMPER=$(oc get pods -o json | jq -r '.items[].metadata|select(.name|test("dumper")).name')
-        export WDQS=$(oc get pods -o json | jq -r '.items[].metadata|select((.name|test("wdqs")) and (.name|test("frontend|proxy|updater")|not)).name')
-        echo "3    0       *       *       *       oc exec $DUMPER -- restore "'$(ls -t /backup_racine/prod/*.gz | head -n1) $DB_TYPE $DB_HOST $DB_NAME $DB_USER $DB_PASS 3306'" >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
-        echo "4    0       *       *       *       oc exec $WDQS -- sh -c 'rm -f data/data.jnl' >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
-        echo "5    0       *       *       *       oc exec $WDQS -- bash -c '/wdqs/runUpdate.sh "'-h http://$WDQS_HOST:$WDQS_PORT -- --wikibaseUrl $WIKIBASE_SCHEME://$WIKIBASE_HOST --conceptUri $WIKIBASE_SCHEME://$WIKIBASE_HOST --entityNamespaces ${WDQS_ENTITY_NAMESPACES} -s 20200501000000'"'" "| tee logfile | awk '1;/Got no real changes/{exit}'"" " >> /var/spool/cron/crontabs/root
-        ;;
-esac
+# usage: /oc.sh <project_name> <dumper|wdqs-jnl|wdqs-run|all> <now>
+
+if [[ "$3" == 'now']];
+    then
+        case $2 in
+            dumper)
+                export DUMPER=$(oc get pods -o json | jq -r '.items[].metadata|select(.name|test("dumper")).name')
+                oc exec $DUMPER -- bash -c 'restore $(ls -t /backup_racine/prod/*.gz | head -n1) $DB_TYPE $DB_HOST $DB_NAME $DB_USER $DB_PASS 3306 >/dev/null 2>&1'
+                ;;
+            wdqs-jnl)
+                export WDQS=$(oc get pods -o json | jq -r '.items[].metadata|select((.name|test("wdqs")) and (.name|test("frontend|proxy|updater")|not)).name')
+                oc exec $WDQS -- sh -c 'rm -f data/data.jnl' >/dev/null 2>&1
+                ;;
+            wdqs-run)
+                export WDQS=$(oc get pods -o json | jq -r '.items[].metadata|select((.name|test("wdqs")) and (.name|test("frontend|proxy|updater")|not)).name')
+                oc exec $WDQS -- bash -c '/wdqs/runUpdate.sh -h http://$WDQS_HOST:$WDQS_PORT -- --wikibaseUrl $WIKIBASE_SCHEME://$WIKIBASE_HOST --conceptUri $WIKIBASE_SCHEME://$WIKIBASE_HOST --entityNamespaces ${WDQS_ENTITY_NAMESPACES} -s 20200501000000' | tee logfile | awk '1;/Got no real changes/{exit}' 
+                ;;
+            all)
+                export DUMPER=$(oc get pods -o json | jq -r '.items[].metadata|select(.name|test("dumper")).name')
+                export WDQS=$(oc get pods -o json | jq -r '.items[].metadata|select((.name|test("wdqs")) and (.name|test("frontend|proxy|updater")|not)).name')
+                oc exec $DUMPER -- restore $(ls -t /backup_racine/prod/*.gz | head -n1) $DB_TYPE $DB_HOST $DB_NAME $DB_USER $DB_PASS 3306 >/dev/null 2>&1
+                oc exec $WDQS -- sh -c 'rm -f data/data.jnl' >/dev/null 2>&1
+                oc exec $WDQS -- bash -c '/wdqs/runUpdate.sh -h http://$WDQS_HOST:$WDQS_PORT -- --wikibaseUrl $WIKIBASE_SCHEME://$WIKIBASE_HOST --conceptUri $WIKIBASE_SCHEME://$WIKIBASE_HOST --entityNamespaces ${WDQS_ENTITY_NAMESPACES} -s 20200501000000' | tee logfile | awk '1;/Got no real changes/{exit}' 
+                ;;
+        esac
+    else
+        echo "3    0       *       *       *       /oc_cron.sh dumper" >> /var/spool/cron/crontabs/root
+        echo "4    0       *       *       *       /oc_cron.sh wdqs-jnl" >> /var/spool/cron/crontabs/root
+        echo "5    0       *       *       *       /oc_cron.sh wdqs-run" >> /var/spool/cron/crontabs/root
+fi
