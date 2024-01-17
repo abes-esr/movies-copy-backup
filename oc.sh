ea772b82380391453b38bad458591a88c25196c6
Nathanaël Blanchet <blanchet@abes.fr>
2024-01-17 18:32:48 +0100

    Update oc.sh

--- oc.sh
+++ oc.sh
@@ -8,24 +8,15 @@ if [[ "$3" == 'now']];
     then
         case $2 in
             dumper)
-                export DUMPER=$(oc get pods -o json | jq -r '.items[].metadata|select(.name|test("dumper")).name')
-                oc exec $DUMPER -- bash -c 'restore $(ls -t /backup_racine/prod/*.gz | head -n1) $DB_TYPE $DB_HOST $DB_NAME $DB_USER $DB_PASS 3306 >/dev/null 2>&1'
-                ;;
+                /oc_cron.sh dumper;;
             wdqs-jnl)
-                export WDQS=$(oc get pods -o json | jq -r '.items[].metadata|select((.name|test("wdqs")) and (.name|test("frontend|proxy|updater")|not)).name')
-                oc exec $WDQS -- sh -c 'rm -f data/data.jnl' >/dev/null 2>&1
-                ;;
+                /oc_cron.sh wdqs-jnl;;
             wdqs-run)
-                export WDQS=$(oc get pods -o json | jq -r '.items[].metadata|select((.name|test("wdqs")) and (.name|test("frontend|proxy|updater")|not)).name')
-                oc exec $WDQS -- bash -c '/wdqs/runUpdate.sh -h http://$WDQS_HOST:$WDQS_PORT -- --wikibaseUrl $WIKIBASE_SCHEME://$WIKIBASE_HOST --conceptUri $WIKIBASE_SCHEME://$WIKIBASE_HOST --entityNamespaces ${WDQS_ENTITY_NAMESPACES} -s 20200501000000' | tee logfile | awk '1;/Got no real changes/{exit}' 
-                ;;
+                /oc_cron.sh wdqs-run;;
             all)
-                export DUMPER=$(oc get pods -o json | jq -r '.items[].metadata|select(.name|test("dumper")).name')
-                export WDQS=$(oc get pods -o json | jq -r '.items[].metadata|select((.name|test("wdqs")) and (.name|test("frontend|proxy|updater")|not)).name')
-                oc exec $DUMPER -- restore $(ls -t /backup_racine/prod/*.gz | head -n1) $DB_TYPE $DB_HOST $DB_NAME $DB_USER $DB_PASS 3306 >/dev/null 2>&1
-                oc exec $WDQS -- sh -c 'rm -f data/data.jnl' >/dev/null 2>&1
-                oc exec $WDQS -- bash -c '/wdqs/runUpdate.sh -h http://$WDQS_HOST:$WDQS_PORT -- --wikibaseUrl $WIKIBASE_SCHEME://$WIKIBASE_HOST --conceptUri $WIKIBASE_SCHEME://$WIKIBASE_HOST --entityNamespaces ${WDQS_ENTITY_NAMESPACES} -s 20200501000000' | tee logfile | awk '1;/Got no real changes/{exit}' 
-                ;;
+                /oc_cron.sh dumper
+                /oc_cron.sh wdqs-jnl
+                /oc_cron.sh wdqs-run;;
         esac
     else
         echo "3    0       *       *       *       /oc_cron.sh dumper" >> /var/spool/cron/crontabs/root
