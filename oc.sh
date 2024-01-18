65ec4b1d235d5aedfa7826b925569a0f1a2018b0
Nathanaël Blanchet <blanchet@abes.fr>
2024-01-18 19:36:37 +0100

    Update oc.sh

--- oc.sh
+++ oc.sh
@@ -11,15 +11,19 @@ if [[ "$3" == 'now' ]];
                 /oc_cron.sh dumper;;
             wdqs-jnl)
                 /oc_cron.sh wdqs-jnl;;
+            wdqs-restart)
+                /oc_cron.sh wdqs-restart
             wdqs-run)
                 /oc_cron.sh wdqs-run;;
             all)
                 /oc_cron.sh dumper
                 /oc_cron.sh wdqs-jnl
+                /oc_cron.sh wdqs-restart
                 /oc_cron.sh wdqs-run;;
         esac
     else
         echo "3    0       *       *       *       /oc_cron.sh dumper" >> /var/spool/cron/crontabs/root
         echo "4    0       *       *       *       /oc_cron.sh wdqs-jnl" >> /var/spool/cron/crontabs/root
-        echo "5    0       *       *       *       /oc_cron.sh wdqs-run" >> /var/spool/cron/crontabs/root
+        echo "5    0       *       *       *       /oc_cron.sh wdqs-restart" >> /var/spool/cron/crontabs/root
+        echo "6    0       *       *       *       /oc_cron.sh wdqs-run" >> /var/spool/cron/crontabs/root
 fi
