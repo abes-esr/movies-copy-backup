aa0723a2b2bb7e24793c27a37ebef3de98c11a00
Nathanaël Blanchet <blanchet@abes.fr>
2024-04-29 14:58:08 +0200

    Update oc_cron.sh
    $DB => $DB_01

--- oc_cron.sh
+++ oc_cron.sh
@@ -3,7 +3,7 @@
 case $1 in
 	dumper)
     		export DUMPER=$(oc get pods -o json | jq -r '.items[].metadata|select(.name|test("dumper")).name')
-		oc exec $DUMPER -- bash -c 'restore $(ls -t /backup_racine/prod/*.gz | head -n1) $DB_TYPE $DB_HOST $DB_NAME $DB_USER $DB_PASS 3306' >/dev/null 2>&1
+		oc exec $DUMPER -- bash -c 'restore $(ls -t /backup_racine/prod/*.gz | head -n1)  $DB01_TYPE $DB01_HOST $DB01_NAME $DB01_USER $DB01_PASS 3306' >/dev/null 2>&1
   		echo "$1 OK"
 		;;
 	wdqs-jnl)
